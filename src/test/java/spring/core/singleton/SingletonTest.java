cb1fac1292e54534e324160eb090ed7eeebd952f
hyeonho <6190606@naver.com>
2022-07-31 13:12:42 +0900

    spring-singleton-container

--- src/test/java/spring/core/singleton/SingletonTest.java
+++ src/test/java/spring/core/singleton/SingletonTest.java
@@ -2,6 +2,8 @@ package spring.core.singleton;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.core.AppConfig;
 import spring.core.member.MemberService;
 
@@ -37,5 +39,21 @@ public class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
         // same -> ==
         // equal -> equals()
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 1. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조 값이 다른지 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService != memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
